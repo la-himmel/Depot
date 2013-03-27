3a9b4996f96b504a884ae3a9a5b7811ccaecb9d9
Ekaterina Zhmud <Ekaterina.Zhmud@noveogroup.com>
2013-03-27 14:32:31 +0700

    cart cleaning was added; new readme

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -41,15 +41,15 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     @cart = current_cart
-    product = Product.find(params[:product_id])
-    
+    product = Product.find(params[:product_id])    
     @line_item = @cart.add_product(product.id) 
+
     #@line_item = @cart.line_items.build
     #@line_item.product = product   
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
         format.xml { render :xml => @line_item, :status => :created, :location => @line_item }
         #format.json { render json: @line_item, status: :created, location: @line_item }
       else
