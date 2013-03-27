c65dcee6f69fc58bb393c16d366fb06670bffc5e
Ekaterina Zhmud <Ekaterina.Zhmud@noveogroup.com>
2013-03-27 17:47:39 +0700

    deletion of single item was added

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -79,12 +79,15 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
+    logger.debug "destroy call"
+
     @line_item = LineItem.find(params[:id])
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to line_items_url }
+      format.html { redirect_to current_cart }
       format.json { head :no_content }
     end
   end
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot1::Application.initialize!
+
+config.logger = Logger.new(STDOUT)
