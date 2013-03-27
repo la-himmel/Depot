28884fa0e7a6adf1bc52eafde306be3abde4162b
Ekaterina Zhmud <Ekaterina.Zhmud@noveogroup.com>
2013-03-27 11:22:47 +0700

    times were added

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -42,9 +42,10 @@ class LineItemsController < ApplicationController
   def create
     @cart = current_cart
     product = Product.find(params[:product_id])
-    # @line_item = @cart.line_items.build(:product => product)   
-    @line_item = @cart.line_items.build
-    @line_item.product = product   
+    
+    @line_item = @cart.add_product(product.id) 
+    #@line_item = @cart.line_items.build
+    #@line_item.product = product   
 
     respond_to do |format|
       if @line_item.save
