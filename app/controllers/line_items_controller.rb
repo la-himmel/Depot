03c7675d84ff927cda4db7059772b85197203589
Yekaterina Zhmud <yekaterina.zhmud@gmail.com>
2013-04-04 16:02:33 +0700

    changed tests so everything is okay

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -86,7 +86,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to current_cart }
+      format.html { redirect_to line_items_url }
       format.json { head :no_content }
     end
   end
